7c0eefcc30ac4f3f0001fad20c3ec354f73c5dbf
Guilherme Marques <marquesguilherme3@gmail.com>
2025-03-03 19:09:17 -0300

    Add subject seeder

--- src/database/seeder/seeder.module.ts
+++ src/database/seeder/seeder.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostgresModule } from 'src/database/postgres/postgres.module';
 import { AchievementsSeederModule } from './achievements/achievements.module';
 import { Seeder } from './seeder';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
-    imports: [PostgresModule, AchievementsSeederModule],
+    imports: [PostgresModule, AchievementsSeederModule, SubjectsModule],
     providers: [Seeder]
 })
 export class SeederModule {}
--- src/database/seeder/seeder.ts
+++ src/database/seeder/seeder.ts
@@ -1,16 +1,21 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { AchievementsSeederService } from "./achievements/achievements.service";
+import { SubjectsSeederService } from "./subjects/subjects.service";
 
 @Injectable()
 export class Seeder {
     private readonly logger = new Logger(Seeder.name)
 
-    constructor(private achievementService: AchievementsSeederService) {}
+    constructor(
+        private achievementService: AchievementsSeederService,
+        private subjectsService: SubjectsSeederService
+    ) {}
 
     async seed() {
         this.logger.log('Beginning seeding process...')
         try {
             await this.achievementService.create()
+            await this.subjectsService.create()
         } catch (error) {
             this.logger.error('Error during seeding process.', error)
         }
